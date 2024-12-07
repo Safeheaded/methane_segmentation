f2a8510739e936ca35f7a73b679f7ec2e448bdb6
Patryk Marczak <m.patryk@outlook.com>
2024-12-07 21:35:50 +0100

    changes to unet, displays prediction

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from lightning.pytorch import Trainer
 from terrain_segmentation.roboflow import RoboflowClient
 from terrain_segmentation.datamodules.default_datamodule import DefaultDatamodule
 from terrain_segmentation.models.default_model import YOLOv5SegmentationModel
-# import ssl
-# ssl._create_default_https_context = ssl._create_unverified_context
+import ssl
+import torch
+import matplotlib.pyplot as plt
+ssl._create_default_https_context = ssl._create_unverified_context
 
 load_dotenv()
 
@@ -12,8 +14,36 @@ def main():
     data_module = DefaultDatamodule()
     model = YOLOv5SegmentationModel(num_classes=1)
 
-    trainer = Trainer(max_epochs=10)
+    trainer = Trainer(max_epochs=20, accelerator='gpu')
     trainer.fit(model=model, datamodule=data_module)
+
+    data_module.setup(stage='test')
+    test_dataset = data_module.test_dataloader().dataset
+    first_image, first_mask = test_dataset[0]
+    model_path = "yolov5_segmentation_model2.pth"
+    torch.save(model.state_dict(), model_path)
+    model.eval()
+    model.freeze()
+    
+    with torch.no_grad():
+        first_image = first_image.unsqueeze(0)  # Dodaj wymiar batch
+        prediction = model(first_image)
+        prediction = prediction.squeeze(0).squeeze(0)
+
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.title("Original Image")
+    plt.imshow(first_image.squeeze().permute(1, 2, 0).cpu().numpy())
+
+    plt.subplot(1, 3, 2)
+    plt.title("Ground Truth Mask")
+    plt.imshow(first_mask.cpu().numpy(), cmap='gray')
+
+    plt.subplot(1, 3, 3)
+    plt.title("Predicted Mask")
+    plt.imshow(prediction.cpu().numpy(), cmap='gray')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- terrain_segmentation/models/default_model.py
+++ terrain_segmentation/models/default_model.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from ultralytics import YOLO
 import inference
 import torchmetrics
+import segmentation_models_pytorch as smp
 
 class YOLOv5SegmentationModel(pl.LightningModule):
     def __init__(self, num_classes, input_channels=3, learning_rate=1e-3):
@@ -14,12 +15,17 @@ class YOLOv5SegmentationModel(pl.LightningModule):
         self.learning_rate = learning_rate
         
         # Ładujemy pretrenowany model YOLOv5 z detekcją
-        self.network = YOLO('yolo11n-seg.pt').model
-        print(self.network)
-        self.save_hyperparameters()
+        self.network = smp.Unet(
+                encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+                encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+                in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+                classes=1,                      # model output channels (number of classes in your dataset)
+            )
+        # print(self.network)
 
         self.loss_function = nn.BCEWithLogitsLoss()
         self.accuracy = torchmetrics.Accuracy(task='binary')
+        self.save_hyperparameters()
         # print(self.model)
         
         # Modyfikacja modelu do segmentacji
@@ -27,26 +33,33 @@ class YOLOv5SegmentationModel(pl.LightningModule):
         # self.model.segmentation_head = nn.Conv2d(self.model.fpn_out_channels[-1], self.num_classes, kernel_size=1)
         
     def forward(self, x):
-        x.permute(0, 3, 1, 2)
-        # Zwykłe przewidywanie, ale z uwzględnieniem segmentacji
-        return self.network(x)['masks']  # Powróć maski obiektów
+        x = x.float()
+        
+        pred = self.network(x)
+        return pred
 
     def training_step(self, batch, batch_idx):
-        images, masks = batch
-        outputs = self(images)  # przewidywania segmentacji
-        loss = self.compute_loss(outputs, masks)
+        inputs, labels = batch
+        outputs = self(inputs)
+        outputs = outputs.squeeze(1)
+
+        loss = self.loss_function(outputs, labels.float())
+
+        self.accuracy.update(outputs, labels.float())
+        self.log('train_loss', loss, prog_bar=True)
+        self.log('train_acc', self.accuracy, prog_bar=True)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
-        images, masks = batch
-        outputs = self(images)
-        loss = self.compute_loss(outputs, masks)
-        return loss
+        inputs, labels = batch
+        outputs = self(inputs)
+        outputs = outputs.squeeze(1)
+        loss = self.loss_function(outputs, labels.float())
 
-    def compute_loss(self, outputs, targets):
-        # Definiujemy loss dla segmentacji, np. BCE z logitami
-        loss = F.binary_cross_entropy_with_logits(outputs, targets)
-        return loss
+        self.accuracy.update(outputs, labels.float())
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', self.accuracy, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
