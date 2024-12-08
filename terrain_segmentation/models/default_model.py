3a842e2dea8f3456dbbdf5630a42dcb499d4fad0
Patryk Marczak <m.patryk@outlook.com>
2024-12-08 13:00:14 +0100

    adds better model stats, optimizer

--- terrain_segmentation/models/default_model.py
+++ terrain_segmentation/models/default_model.py
@@ -1,66 +1,134 @@
 import torch
-import torch.nn as nn
 import lightning.pytorch as pl
-import torch.nn.functional as F
-from ultralytics import YOLO
-import inference
 import torchmetrics
 import segmentation_models_pytorch as smp
+from torch.optim import lr_scheduler
 
 class YOLOv5SegmentationModel(pl.LightningModule):
-    def __init__(self, num_classes, input_channels=3, learning_rate=1e-3):
+    def __init__(self, num_classes, input_channels=3, learning_rate=1e-3, encoder_name="resnet34", T_MAX=100):
         super().__init__()
+        self.T_MAX = T_MAX
         self.num_classes = num_classes
         self.input_channels = input_channels
         self.learning_rate = learning_rate
         
         # Ładujemy pretrenowany model YOLOv5 z detekcją
         self.network = smp.Unet(
-                encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+                encoder_name=encoder_name,        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
                 encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
                 in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
                 classes=1,                      # model output channels (number of classes in your dataset)
             )
-        # print(self.network)
+        
+        params = smp.encoders.get_preprocessing_params(encoder_name)
+        self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
 
-        self.loss_function = nn.BCEWithLogitsLoss()
+        self.loss_function = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
         self.accuracy = torchmetrics.Accuracy(task='binary')
         self.save_hyperparameters()
-        # print(self.model)
+
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+        self.test_step_outputs = []
         
-        # Modyfikacja modelu do segmentacji
-        # Zakładając, że chcemy dodać głowę segmentacji na końcu
-        # self.model.segmentation_head = nn.Conv2d(self.model.fpn_out_channels[-1], self.num_classes, kernel_size=1)
-        
-    def forward(self, x):
-        x = x.float()
-        
-        pred = self.network(x)
-        return pred
+    def forward(self, image):
+        image = (image - self.mean) / self.std
+        return self.network(image)
 
     def training_step(self, batch, batch_idx):
-        inputs, labels = batch
-        outputs = self(inputs)
-        outputs = outputs.squeeze(1)
+        train_loss_info = self.shared_step(batch)
 
-        loss = self.loss_function(outputs, labels.float())
+        self.training_step_outputs.append(train_loss_info)
 
-        self.accuracy.update(outputs, labels.float())
-        self.log('train_loss', loss, prog_bar=True)
-        self.log('train_acc', self.accuracy, prog_bar=True)
-
-        return loss
+        return train_loss_info
+    
+    def on_train_epoch_end(self):
+        self.shared_epoch_end(self.training_step_outputs, "train")
+        # empty set output list
+        self.training_step_outputs.clear()
+        return
 
     def validation_step(self, batch, batch_idx):
+        valid_loss_info = self.shared_step(batch)
+
+        self.validation_step_outputs.append(valid_loss_info)
+
+        return valid_loss_info
+    
+    def on_validation_epoch_end(self):
+        self.shared_epoch_end(self.validation_step_outputs, "valid")
+        self.validation_step_outputs.clear()
+        return
+    
+    def test_step(self, batch, batch_idx):
+        test_loss_info = self.shared_step(batch)
+        self.test_step_outputs.append(test_loss_info)
+        return test_loss_info
+
+    def on_test_epoch_end(self):
+        self.shared_epoch_end(self.test_step_outputs, "test")
+        # empty set output list
+        self.test_step_outputs.clear()
+        return
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.T_MAX, eta_min=1e-5)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+                "frequency": 1,
+            },
+        }
+        return
+    
+    def shared_step(self, batch):
         inputs, labels = batch
         outputs = self(inputs)
         outputs = outputs.squeeze(1)
+
         loss = self.loss_function(outputs, labels.float())
 
-        self.accuracy.update(outputs, labels.float())
-        self.log('val_loss', loss, prog_bar=True)
-        self.log('val_acc', self.accuracy, prog_bar=True)
+        prob_mask =  outputs.sigmoid()
+        pred_mask = (prob_mask > 0.5).float()
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+        tp, fp, fn, tn = smp.metrics.get_stats(
+            pred_mask.long(), labels.long(), mode="binary"
+        )
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+    
+    def shared_epoch_end(self, outputs, stage):
+        # aggregate step metics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # per image IoU means that we first calculate IoU score for each image
+        # and then compute mean over these scores
+        per_image_iou = smp.metrics.iou_score(
+            tp, fp, fn, tn, reduction="micro-imagewise"
+        )
+
+        # dataset IoU means that we aggregate intersection and union over whole dataset
+        # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
+        # in this particular case will not be much, however for dataset
+        # with "empty" images (images without target class) a large gap could be observed.
+        # Empty images influence a lot on per_image_iou and much less on dataset_iou.
+        dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+
+        self.log_dict(metrics, prog_bar=True)
