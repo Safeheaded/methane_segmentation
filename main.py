ffa998962f50a8ce62f36f062cfefd32be1a2c24
Patryk Marczak <m.patryk@outlook.com>
2024-12-08 12:59:58 +0100

    adds predition script

--- main.py
+++ main.py
@@ -12,38 +12,18 @@ load_dotenv()
 
 def main():
     data_module = DefaultDatamodule()
-    model = YOLOv5SegmentationModel(num_classes=1)
 
-    trainer = Trainer(max_epochs=20, accelerator='gpu')
+    EPOCHS = 5
+    T_MAX = EPOCHS * 15
+
+    model = YOLOv5SegmentationModel(num_classes=1, T_MAX=T_MAX)
+
+    trainer = Trainer(max_epochs=EPOCHS, accelerator='gpu')
     trainer.fit(model=model, datamodule=data_module)
 
-    data_module.setup(stage='test')
-    test_dataset = data_module.test_dataloader().dataset
-    first_image, first_mask = test_dataset[0]
-    model_path = "yolov5_segmentation_model2.pth"
+    model_path = "yolov5_segmentation_model3.pth"
     torch.save(model.state_dict(), model_path)
-    model.eval()
-    model.freeze()
-    
-    with torch.no_grad():
-        first_image = first_image.unsqueeze(0)  # Dodaj wymiar batch
-        prediction = model(first_image)
-        prediction = prediction.squeeze(0).squeeze(0)
 
-    plt.figure(figsize=(15, 5))
-    plt.subplot(1, 3, 1)
-    plt.title("Original Image")
-    plt.imshow(first_image.squeeze().permute(1, 2, 0).cpu().numpy())
-
-    plt.subplot(1, 3, 2)
-    plt.title("Ground Truth Mask")
-    plt.imshow(first_mask.cpu().numpy(), cmap='gray')
-
-    plt.subplot(1, 3, 3)
-    plt.title("Predicted Mask")
-    plt.imshow(prediction.cpu().numpy(), cmap='gray')
-
-    plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ predict.py
@@ -0,0 +1,35 @@
+import torch
+import matplotlib.pyplot as plt
+from terrain_segmentation.models.default_model import YOLOv5SegmentationModel
+from terrain_segmentation.datasets.default_dataset import DefaultDataset
+
+def main():
+    model_path = "yolov5_segmentation_model3.pth"
+    model = YOLOv5SegmentationModel(1, T_MAX=2*15)
+    model.load_state_dict(torch.load(model_path))
+    test_dataset = DefaultDataset("datasets/Segmentacja terenów --1/test/images", "datasets/Segmentacja terenów --1/test/labels")
+    first_image, first_mask = test_dataset[0]
+    with torch.no_grad():
+        first_image = first_image.unsqueeze(0)  # Dodaj wymiar batch
+        prediction = model(first_image)
+        prediction = prediction.squeeze(0).squeeze(0)
+        prediction = prediction.sigmoid()
+        prediction = (prediction > 0.5).float()
+
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.title("Original Image")
+    plt.imshow(first_image.squeeze().permute(1, 2, 0).cpu().numpy())
+
+    plt.subplot(1, 3, 2)
+    plt.title("Ground Truth Mask")
+    plt.imshow(first_mask.cpu().numpy(), cmap='gray')
+
+    plt.subplot(1, 3, 3)
+    plt.title("Predicted Mask")
+    plt.imshow(prediction.cpu().numpy(), cmap='gray')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
