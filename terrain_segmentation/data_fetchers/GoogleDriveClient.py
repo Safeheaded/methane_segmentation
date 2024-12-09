b3a8e2c82df5ee0948566a3fd17b5c79ebd8c611
Patryk Marczak <m.patryk@outlook.com>
2024-12-09 19:20:23 +0100

    fetching files from google drive

---
+++ terrain_segmentation/data_fetchers/GoogleDriveClient.py
@@ -0,0 +1,22 @@
+import gdown
+from pathlib import Path
+import os
+import zipfile
+
+class GoogleDriveClient:
+    def __init__(self, file_id: str = os.getenv("GOOGLE_DRIVE_FILE_ID")):
+        self.file_id = file_id
+
+    def getDataset(self):
+        url = f'https://drive.google.com/uc?id={self.file_id}'
+
+        path = Path(os.path.join(os.getcwd(), "datasets"))
+
+        if not path.exists():
+            path.mkdir()
+
+        output = path / 'pan_geodeta.zip'
+        gdown.download(url, str(output), quiet=False)
+        with zipfile.ZipFile(output, 'r') as zip_ref:
+            zip_ref.extractall(path)
+            os.remove(output)
