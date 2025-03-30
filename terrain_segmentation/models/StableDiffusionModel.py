b3bc25053fe14a4d74b90f2c113ed14a8ced08a4
Patryk Marczak <m.patryk@outlook.com>
2025-03-30 21:11:24 +0200

    adds sample images uploads on validation epoch end

--- terrain_segmentation/models/StableDiffusionModel.py
+++ terrain_segmentation/models/StableDiffusionModel.py
@@ -14,14 +14,14 @@ from lightning.pytorch.loggers import NeptuneLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
-from diffusers import AutoencoderKL, UNet2DConditionModel, DDPMScheduler, UNet2DModel
+from diffusers import AutoencoderKL, UNet2DConditionModel, DDPMScheduler, UNet2DModel, DDPMPipeline
 
 class StableDiffusionModel(pl.LightningModule):
     def __init__(self, learning_rate=1e-5):
         super().__init__()
         # self.vae = AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="vae").to('mps')
         self.unet = UNet2DModel(
-            sample_size=512,
+            sample_size=128,
             in_channels=19,
             out_channels=19,
             layers_per_block=2,
@@ -46,7 +46,6 @@ class StableDiffusionModel(pl.LightningModule):
         self.noise_scheduler = DDPMScheduler(num_train_timesteps=1000, beta_schedule='linear')
         self.learning_rate = learning_rate
 
-        self.example_input = None
         self.pred_logged_epochs = set()
 
         # self.vae.encoder.conv_in = torch.nn.Conv2d(18, 128, kernel_size=3, stride=1, padding=1)
@@ -78,36 +77,53 @@ class StableDiffusionModel(pl.LightningModule):
 
         model_output = self(noisy_images, timesteps)
 
-        if self.example_input is None and loss_name == "val_loss":
-            self.example_input = model_output[0]
-
         loss = F.mse_loss(model_output.float(), noise.float())
         self.log(loss_name, loss, prog_bar=True)
         return loss
     
     def on_validation_epoch_end(self):
-        if (self.current_epoch + 1) % 10 == 0 and self.current_epoch not in self.pred_logged_epochs:
+        if (self.current_epoch + 1) % 1 == 0 and self.current_epoch not in self.pred_logged_epochs:
             self.pred_logged_epochs.add(self.current_epoch)
 
-            first_image = self.example_input.cpu().detach().numpy()
+            with torch.no_grad():
+                pipeline = DDPMPipeline(
+                    unet=self.unet,
+                    scheduler=self.noise_scheduler,
+                )
+                generator = torch.Generator(device='cpu').manual_seed(0)
+                images = pipeline(
+                    generator=generator,
+                    batch_size=1,
+                    num_inference_steps=1000,
+                    output_type="np",
+                ).images
 
-            fig, axes = plt.subplots(4, 5, figsize=(15, 12))
-            axes = axes.flatten()
+                # fig, axes = plt.subplots(figsize=(6, 6))
 
-            # Iterujemy po wszystkich 18 kanałach
-            for i in range(19):
-                # Wyciągamy i-ty kanał: shape (512, 512)
-                channel = first_image[i, ...]
-                axes[i].imshow(channel, cmap='gray')
-                axes[i].set_title(f'Channel {i}')
-                axes[i].axis('off')
+                # Iterujemy po wszystkich 18 kanałach
+                # for i in range(19):
+                    # Wyciągamy i-ty kanał: shape (512, 512)
 
-            plt.tight_layout()
-            self.logger.experiment[f"valid/prediction"].append(fig)
-            fig.clear()
-            plt.close(fig)
+                images = (images * 255).round().astype("uint8")
 
-        self.example_input = None
+                first_image = images[0]
+
+                # Przygotuj grid – np. 3 wiersze x 6 kolumn (czyli 18 subplots)
+                fig, axes = plt.subplots(3, 6, figsize=(15, 8))
+                axes = axes.flatten()
+
+                # Iterujemy po wszystkich 18 kanałach
+                for i in range(18):
+                    # Wyciągamy i-ty kanał: shape (512, 512)
+                    channel = first_image[..., i]
+                    axes[i].imshow(channel, cmap='gray')
+                    axes[i].set_title(f'Channel {i}')
+                    axes[i].axis('off')
+
+                plt.tight_layout()
+                self.logger.experiment[f"valid/prediction"].append(fig)
+                fig.clear()
+                plt.close(fig)
 
 
         return super().on_validation_epoch_end()
