80515d8dd5688ec8d2704e306ae397a4bdfd5912
Patryk Marczak <m.patryk@outlook.com>
2025-04-26 11:46:32 +0200

    adds native loss function for acc-unet

--- methane_segmentation/models/acc_unet.py
+++ methane_segmentation/models/acc_unet.py
@@ -4,6 +4,92 @@ ACC-UNet architecture using PyTorch
 from .base_model import BaseModel
 import torch
 import segmentation_models_pytorch as smp
+from torch import nn
+import torch.nn.functional as F
+
+class WeightedBCE(nn.Module):
+
+    def __init__(self, weights=[0.4, 0.6],n_labels=1):
+        super(WeightedBCE, self).__init__()
+        self.weights = weights
+        self.n_labels = n_labels
+
+    def forward(self, logit_pixel, truth_pixel):
+        # print("====",logit_pixel.size())
+        if self.n_labels ==1:
+            logit = logit_pixel.view(-1)
+            truth = truth_pixel.view(-1)
+            assert(logit.shape==truth.shape)
+            loss = F.binary_cross_entropy(logit, truth, reduction='none')
+            pos = (truth>0.5).float()
+            neg = (truth<0.5).float()
+            
+            pos_weight = pos.sum().item() + 1e-12
+            neg_weight = neg.sum().item() + 1e-12
+            loss = (self.weights[0]*pos*loss/pos_weight + self.weights[1]*neg*loss/neg_weight).sum()
+
+            return loss
+        
+
+class WeightedDiceLoss(nn.Module):
+    def __init__(self, weights=[0.5, 0.5],n_labels=1): # W_pos=0.8, W_neg=0.2
+        super(WeightedDiceLoss, self).__init__()
+        self.weights = weights
+        self.n_labels = n_labels
+
+    def forward(self, logit, truth, smooth=1e-5):
+
+        if(self.n_labels==1):
+            batch_size = len(logit)
+            logit = logit.view(batch_size,-1)
+            truth = truth.view(batch_size,-1)
+            assert(logit.shape==truth.shape)
+            p = logit.view(batch_size,-1)
+            t = truth.view(batch_size,-1)
+            w = truth.detach()
+            w = w*(self.weights[1]-self.weights[0])+self.weights[0]
+            # p = w*(p*2-1)  #convert to [0,1] --> [-1, 1]
+            # t = w*(t*2-1)
+            p = w*(p)
+            t = w*(t)
+            intersection = (p * t).sum(-1)
+            union =  (p * p).sum(-1) + (t * t).sum(-1)
+            dice  = 1 - (2*intersection + smooth) / (union +smooth)
+            # print "------",dice.data
+
+            loss = dice.mean()
+            return loss
+
+class WeightedDiceBCE(nn.Module):
+    def __init__(self,dice_weight=1,BCE_weight=1,n_labels=1):
+        super(WeightedDiceBCE, self).__init__()
+        self.BCE_loss = WeightedBCE(weights=[0.5, 0.5],n_labels=n_labels)
+        self.dice_loss = WeightedDiceLoss(weights=[0.5, 0.5],n_labels=n_labels)
+        self.n_labels = n_labels
+        self.BCE_weight = BCE_weight
+        self.dice_weight = dice_weight
+
+    def _show_dice(self, inputs, targets):
+        inputs[inputs>=0.5] = 1
+        inputs[inputs<0.5] = 0
+        # print("2",np.sum(tmp))
+        targets[targets>0] = 1
+        targets[targets<=0] = 0
+        hard_dice_coeff = 1.0 - self.dice_loss(inputs, targets)
+        return hard_dice_coeff
+
+    def forward(self, inputs, targets):
+        # inputs = inputs.contiguous().view(-1)
+        # targets = targets.contiguous().view(-1)
+        # print "dice_loss", self.dice_loss(inputs, targets)
+        # print "focal_loss", self.focal_loss(inputs, targets)
+        dice = self.dice_loss(inputs, targets)
+        BCE = self.BCE_loss(inputs, targets)
+        # print "dice",dice
+        # print "focal",focal
+        dice_BCE_loss = self.dice_weight * dice + self.BCE_weight * BCE
+
+        return dice_BCE_loss
 
 
 class ChannelSELayer(torch.nn.Module):
@@ -597,7 +683,8 @@ class AccUnet(BaseModel):
             self.out = torch.nn.Conv2d(n_filts, n_classes + 1, kernel_size=(1, 1))
             self.last_activation = None
 
-        self.loss_function = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+        self.loss_function = WeightedDiceBCE(dice_weight=0.5,BCE_weight=0.5, n_labels=1)
+        # self.loss_function = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
         self.save_hyperparameters()
 
     def forward(self, x):
